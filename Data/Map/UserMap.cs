1cb1e19a2da1a8294945a7fc4ca6b61709e88cca
Rafaelsn11 <faelsantana001@gmail.com>
2025-01-07 23:59:41 -0300

    feat: adding properties Password and Salt of the user entity

--- Data/Map/UserMap.cs
+++ Data/Map/UserMap.cs
@@ -16,6 +16,8 @@ public class UserMap : BaseMap<User>
 
         builder.Property(x => x.Name).HasColumnName("name").IsRequired();
         builder.Property(x => x.Email).HasColumnName("email").IsRequired();
+        builder.Property(x => x.Password).HasColumnName("password").IsRequired();
+        builder.Property(x => x.Salt).HasColumnName("salt").IsRequired();
         builder.Property(x => x.BirthDate).HasColumnName("birth_date").IsRequired();
         builder.Property(x => x.IsActive).HasColumnName("is_active");
         builder.HasKey(x => x.Id);
--- Models/Entities/User.cs
+++ Models/Entities/User.cs
@@ -5,6 +5,8 @@ public class User
     public Guid Id { get; set; }
     public string Name { get; set; }
     public string Email { get; set; }
+    public string Password { get; set; }
+    public string Salt { get; set; }
     public DateTime BirthDate { get; set; }
     public bool IsActive { get; set; }
     public List<Edition> Loans { get; set; }
