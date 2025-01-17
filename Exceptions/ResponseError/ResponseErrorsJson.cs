5c9ceb6dc1718b4817c383b03e0f3b96176bfe6d
Rafaelsn11 <faelsantana001@gmail.com>
2025-01-16 23:01:47 -0300

    feat and refactor: exception for Attibutes Authentication and adding constructor for exception ResponseErrorJson

--- Exceptions/ResponseError/ResponseErrorsJson.cs
+++ Exceptions/ResponseError/ResponseErrorsJson.cs
@@ -2,9 +2,19 @@ namespace LibraNet.Exceptions.ResponseError;
 
 public class ResponseErrorsJson
 {
+    public IList<string> Errors { get; set; } = [];
+    public bool TokenExpired { get; set; }
+
     public ResponseErrorsJson(IList<string> errors)
     {
         Errors = errors;
     }
-    public IList<string> Errors { get; set; } = [];
+
+    public ResponseErrorsJson(string error)
+    {
+        Errors = new List<string>
+        {
+            error
+        };
+    }
 }
